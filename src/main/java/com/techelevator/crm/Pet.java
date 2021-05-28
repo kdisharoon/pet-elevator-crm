32d2e4efe0d558330fbcb8b0f7e855f501d2a9ad
Joshua Olsen <joshua.olsen14@gmail.com>
2021-05-28 15:20:35 -0400

    Wrote all classes defined by the readme, unit tests still remain to be written.

---
+++ src/main/java/com/techelevator/crm/Pet.java
@@ -0,0 +1,58 @@
+package com.techelevator.crm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pet {
+
+    // ## INSTANCE VARIABLES ##
+
+    private String name;
+    private String species;
+    private List<String> vaccinations = new ArrayList<>();
+
+    // ## GETTERS AND SETTERS ##
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public List<String> getVaccinations() {
+        return vaccinations;
+    }
+
+    public void setVaccinations(List<String> vaccinations) {
+        this.vaccinations = vaccinations;
+    }
+
+    // ## CONSTRUCTORS ##
+
+    public Pet(String name, String species) {
+        this.name = name;
+        this.species = species;
+    }
+
+    // ## METHODS ##
+
+    public String listVaccinations() {
+        String output = "";
+        for (String vax : vaccinations) {
+            output += vax + ", ";
+        }
+        output = output.substring(0, output.length() - 2);
+        return output;
+    }
+
+}
